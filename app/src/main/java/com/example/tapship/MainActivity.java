f04ad1060ef0a168755bb77d100d323f0539f52d
ganpat-patel-012 <ganpat.patel.012@gmail.com>
2021-04-11 00:46:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tapship/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.tapship;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.webkit.WebView;
+import android.os.Bundle;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mywebview = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mywebview.setWebViewClient(new WebViewClient());
+        mywebview.loadUrl("https://www.troupertech.com/");
+
+        mywebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+                try {
+                    webView.stopLoading();
+                } catch (Exception e) {
+                }
+
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                }
+
+                webView.loadUrl("about:blank");
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage("Check your internet connection and try again.");
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Try Again", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+
+                alertDialog.show();
+                super.onReceivedError(webView, errorCode, description, failingUrl);
+            }
+        });
+    }
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+            return true;
+
+        }
+    }
+        @Override
+        // This method is used to detect back button
+        public void onBackPressed () {
+            if (mywebview.canGoBack()) {
+                mywebview.goBack();
+            } else {
+                // Let the system handle the back button
+                super.onBackPressed();
+            }
+        }
+}

3c55b0a87679e322edd027bcd3ef07b2ea2f9754
ganpat-patel-012 <ganpat.patel.012@gmail.com>
2021-06-27 11:40:33 +0530

    Commit Final

--- app/src/main/java/com/example/tapship/MainActivity.java
+++ app/src/main/java/com/example/tapship/MainActivity.java
@@ -1,73 +1,150 @@
 package com.example.tapship;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
-import android.view.Menu;
-import android.view.View;
-import android.view.Window;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.os.Bundle;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class MainActivity extends AppCompatActivity {
-    WebView mywebview;
+
+
+    WebView webView;
     ImageView imageView;
     ProgressBar progressBar;
     TextView textView;
     ActionBar actionBar;
 
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    public String url ="https://tapship.herokuapp.com/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("");
-        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.driver);*/
 
+        getSupportActionBar().setTitle("");
         actionBar=getSupportActionBar();
         actionBar.hide();
 
-        mywebview = (WebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         textView = findViewById(R.id.text);
-        WebSettings webSettings = mywebview.getSettings();
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mywebview.setWebViewClient(new WebViewClient());
-        mywebview.loadUrl("https://tapship.herokuapp.com/");
         imageView = (ImageView) findViewById(R.id.image);
 
-        mywebview.setWebChromeClient(new WebChromeClient() {
+        webView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
                 progressBar.setProgress(progress);
                 if (progress == 100) {
                     progressBar.setVisibility(View.GONE);
                     textView.setVisibility(View.GONE);
-                    mywebview.setVisibility(View.VISIBLE);
+                    webView.setVisibility(View.VISIBLE);
                     actionBar.hide();
 
                 } else {
                     progressBar.setVisibility(View.VISIBLE);
                     textView.setVisibility(View.VISIBLE);
-                    mywebview.setVisibility(View.GONE);
+                    webView.setVisibility(View.GONE);
                     actionBar.hide();
 
                 }
             }
         });
 
-        mywebview.setWebViewClient(new WebViewClient() {
+        webView.setWebViewClient(new myWebClient());
+        webView.setWebChromeClient(new WebChromeClient()
+        {
+            // For 3.0+ Devices (Start)
+            // onActivityResult attached before constructor
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    intent = fileChooserParams.createIntent();
+                }
+                try
+                {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e)
+                {
+                    uploadMessage = null;
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+            {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
             public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
                 try {
                     webView.stopLoading();
@@ -79,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 imageView.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.GONE);
+                webView.setVisibility(View.GONE);
 
                 imageView.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -92,7 +171,14 @@ public class MainActivity extends AppCompatActivity {
                 super.onReceivedError(webView, errorCode, description, failingUrl);
             }
         });
+
+
+
+
+
+
     }
+
     public class myWebClient extends WebViewClient
     {
         @Override
@@ -123,18 +209,50 @@ public class MainActivity extends AppCompatActivity {
             progressBar.setVisibility(View.GONE);
             textView.setVisibility(View.GONE);
             actionBar.hide();
-
         }
     }
-        @Override
-        // This method is used to detect back button
-        public void onBackPressed () {
-            if (mywebview.canGoBack()) {
-                mywebview.goBack();
-            } else {
-                // Let the system handle the back button
-                super.onBackPressed();
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (requestCode == REQUEST_SELECT_FILE)
+            {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
             }
         }
+        else if (requestCode == FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+
+
 
 }
